8e24683cac51b012a17b412c013e2cca845ba772
xg <xg@163.com>
2023-03-03 15:53:59 +0800

    first commit from idea

---
+++ src/main/java/xg/git/GitTest.java
@@ -0,0 +1,11 @@
+package xg.git;
+
+/**
+ * @author XG
+ * @create 2023-03-03 15:51
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
